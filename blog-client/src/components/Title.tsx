d787101464229d4adab2a77b05181fbb2cb2b084
JordyJ0estar <141701811+JordyJ0estar@users.noreply.github.com>
2024-07-29 20:52:30 -0400

    Add(Styles): Add new classname for container

--- blog-client/src/components/Title.tsx
+++ blog-client/src/components/Title.tsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material";
 import Link from "next/link";
+import "./Components.css"
 
 const TitleApi = () => {
     return (  
-        <Typography variant="h5" >
+        <Typography className="containerTitle" >
             <Link href={"/"} className="title">Ejercicio Python con Django usando API</Link>
         </Typography>
     );
